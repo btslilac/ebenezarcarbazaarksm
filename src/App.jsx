fd93382c1954bf9a298da8967aad2b56eb27a6d7
Timothy <timtheesam@gmail.com>
2026-03-25 11:55:41 +0300

    Updated Version

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import Inventory from "./pages/Inventory";
+import Contact from "./pages/Contact";
+import CarHire from "./pages/CarHire";
+import TradeIn from "./pages/TradeIn";
+import VehicleDetails from "./pages/VehicleDetails";
+import VehicleDetailsHire from "./pages/VehicleDetailsHire";
+import Admin from "./pages/Admin";
+
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Navbar />
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/vehicle/:id" element={<VehicleDetails />} />
+          <Route path="/vehicle/hire/:id" element={<VehicleDetailsHire />} />
+          <Route path="/car-hire" element={<CarHire />} />
+          <Route path="/trade-in" element={<TradeIn />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
